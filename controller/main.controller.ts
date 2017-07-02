0617ff426a4eb46ccddc9e30206104ff2c10c3e3
Florian Tischler <f.tischler@pmc-services.de>
2017-07-02 20:51:53 +0200

    added route for /

--- controller/main.controller.ts
+++ controller/main.controller.ts
@@ -1,12 +1,19 @@
 import { injectable } from 'inversify';
 import { RESTController } from '../model/rest-controller';
 import { MitarbeiterController } from './mitarbeiter.controller';
+import { app } from '../app';
 
 @injectable()
 export class MainController implements RESTController {
-    constructor(private mitarbeiterController: MitarbeiterController) { }
+    constructor(private mitarbeiterController: MitarbeiterController) {
+    }
 
     initRoutes(): void {
         this.mitarbeiterController.initRoutes();
+        app.get('/', this.getRunningState);
+    }
+
+    public getRunningState(req?, res?, next?) {
+        res.send('Server is running');
     }
 }
