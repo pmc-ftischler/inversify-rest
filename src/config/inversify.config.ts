4afb40697a82b2e55715e5f0e201b18966b4a782
Florian Tischler <f.tischler@pmc-services.de>
2017-07-09 23:48:19 +0200

    added tags in inversify.config

--- src/config/inversify.config.ts
+++ src/config/inversify.config.ts
@@ -3,10 +3,11 @@ import { interfaces, TYPE } from 'inversify-express-utils';
 import { MitarbeiterController } from '../controller/mitarbeiter.controller';
 import { MitarbeiterService } from '../services/mitarbeiter.service';
 import { MainController } from '../controller/main.controller';
+import { TAGS } from '../constant/tags';
 
 export const appContainer = new Container();
 appContainer.bind<interfaces.Controller>(TYPE.Controller).to(MainController)
-    .whenTargetNamed('MainController');
+    .whenTargetNamed(TAGS.MainController);
 appContainer.bind<interfaces.Controller>(TYPE.Controller).to(MitarbeiterController)
-    .whenTargetNamed('MitarbeiterController');
+    .whenTargetNamed(TAGS.MitarbeiterController);
 appContainer.bind<MitarbeiterService>(MitarbeiterService).toSelf();
---
+++ src/constant/tags.ts
@@ -0,0 +1,4 @@
+export const TAGS = {
+    MainController: 'MainController',
+    MitarbeiterController: 'MitarbeiterController'
+};
